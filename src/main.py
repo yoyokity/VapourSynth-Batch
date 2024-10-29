664fb525e697fd0241eea980a527396e401d43bc
yoyokity <1125404308@qq.com>
2024-10-29 22:02:00 +0800

    添加音频编码

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import os
 import sys
 import configparser
 
-from  pathlib import Path
+from pathlib import Path
 from vpy import Vpy
 
 
@@ -31,17 +31,38 @@ if __name__ == '__main__':
     # 读取配置文件
     config = read_ini('./配置参数.ini')
 
-    #检查exe路径是否正确
+    # 检查exe路径是否正确
     vsPipe_path = config.get('vapoursynth', 'vsPipe_path')
     if not os.path.exists(vsPipe_path):
         print(f"VapourSynth路径错误！请修改配置文件！")
         input("按 Enter 退出...")
         exit()
 
+    # 获取vpy文件路径
+    vpy_file = config.get('vapoursynth', 'vpy_file')
+    if not vpy_file:
+        vpy_file = 'vpy文件.py'
+
+    # 检查vpy文件是否存在
+    if not os.path.exists(vpy_file):
+        print(f"找不到vpy文件！")
+        input("按 Enter 退出...")
+        exit()
+    if not vpy_file.endswith('.py'):
+        print(f"当前vpy文件格式有误，请修改文件后缀为.py！")
+        input("按 Enter 退出...")
+        exit()
+    with open(vpy_file, 'r', encoding='utf-8') as file:
+        content = file.read()
+    if not content.find(Vpy.old_string):
+        print(f"当前vpy文件没有替换字符串 “{Vpy.old_string}” ，请修改文件内容！")
+        input("按 Enter 退出...")
+        exit()
+
     #
     for input_file in input_files:
         print(f"开始编码 {input_file}")
-        vpy = Vpy(input_file)
+        vpy = Vpy(input_file, vpy_file)
         vpy.run(config)
 
     input("编码完成，按 Enter 退出...")
--- src/vpy/__init__.py
+++ src/vpy/__init__.py
@@ -10,8 +10,9 @@ class Vpy:
     destination_file = r"temp\temp.vpy"
     old_string = '$FILE_PATH$'
 
-    def __init__(self, path: str):
+    def __init__(self, path: str, vpy_file: str):
         self.path = path
+        Vpy.source_file = vpy_file
         self.nvencc_path = Path(os.getcwd()).joinpath(r'tools\nvencc\NVEncC64.exe')
         self.ffmpeg_path = Path(os.getcwd()).joinpath(r'tools\ffmpeg\ffmpeg.exe')
 
@@ -34,32 +35,67 @@ class Vpy:
         with open(Vpy.destination_file, 'w', encoding='utf-8') as file:
             file.write(new_content)
 
+    def _get_parameter(self, config: ConfigParser):
+        # vs相关
+        self.vs_vsPipe_path = config.get('vapoursynth', 'vsPipe_path')
+
+        # video相关
+        self.video_output = config.get('video', 'output_path')
+        self.video_output_format = config.get('video', 'output_format')
+        self.video_quality = config.get('video', 'quality')
+        self.video_aq = config.get('video', 'aq')
+
+        # audio相关
+        self.audio_codec = config.get('audio', 'codec')
+        self.audio_bitrate = config.get('audio', 'bitrate')
+        self.audio_samplerate = config.get('audio', 'samplerate')
+
     def run(self, config: ConfigParser):
-        vsPipe_path = config.get('vapoursynth', 'vsPipe_path')
-        output = config.get('video', 'output_path')
-        output_format = config.get('video', 'output_format')
-        quality = config.get('video', 'quality')
-        aq = config.get('video', 'aq')
+        self._get_parameter(config)
 
         input_path, input_filename = os.path.split(self.path)
         input_filename_short = os.path.splitext(input_filename)[0]
 
-        if output == '':
-            output_file = os.path.join(input_path, f'[encoded]{input_filename_short}.{output_format}')
+        if self.video_output == '':
+            output_file = os.path.join(input_path, f'[encoded]{input_filename_short}.{self.video_output_format}')
         else:
-            output_file = os.path.join(output, f'[encoded]{input_filename_short}.{output_format}')
+            output_file = os.path.join(self.video_output, f'[encoded]{input_filename_short}.{self.video_output_format}')
 
-        cmd = [
-            f'{vsPipe_path}', '--y4m', f'{Vpy.destination_file}', '-', '|',
-            f'{self.nvencc_path}', '--y4m', '-i', '-', '-c', 'hevc', '--qvbr', f'{quality}',
-            '--preset', 'p7', '--lookahead', '12', '--output-depth', '10', '--profile', 'main10',
-        ]
 
-        if aq != '':
-            cmd.extend(['--aq', '--aq-strength', f'{aq}'])
+        # 命令
 
-        cmd.extend(['-o', f'{output_file}'])
+        # vspipe
+        cmd = [f'{self.vs_vsPipe_path}', '--y4m', f'{Vpy.destination_file}', '-', '|']
+        #ffmpeg
+        ffmpeg_cmd=[f'{self.ffmpeg_path}','-f','yuv4mpegpipe',
+                    '-i','pipe:',
+                    '-i',f'{self.path}',
+                    '-map','0:v','-map','1:a',
+                    '-c:v','copy','-c:a','copy',
+                    '-f','nut','-','|']
+        # nvencc
+        nvencc_cmd = ['-i', '-', '-c', 'hevc', '--qvbr', f'{self.video_quality}',
+                      '--preset', 'p7', '--lookahead', '12', '--output-depth', '10', '--profile', 'main10']
+        if self.video_aq != '':
+            nvencc_cmd.extend(['--aq', '--aq-strength', f'{self.video_aq}'])
 
+#        判断是否添加音频
+        if self.audio_codec == '':
+            cmd.extend([f'{self.nvencc_path}', '--y4m'])
+            cmd.extend(nvencc_cmd)
+            cmd.extend(['-o', f'{output_file}'])
+        else:
+            cmd.extend(ffmpeg_cmd)
+            cmd.extend([f'{self.nvencc_path}', '--avsw'])
+            cmd.extend(nvencc_cmd)
+            cmd.extend(['--audio-codec',f'{self.audio_codec}',
+                        '--audio-bitrate',f'{self.audio_bitrate}',
+                        '--audio-samplerate',f'{self.audio_samplerate}'])
+            cmd.extend(['-o', f'{output_file}'])
+
+
+        # 开始运行
+        print(" ".join(cmd))
         subprocess.run(cmd, shell=True)
 
         # 删除临时文件
